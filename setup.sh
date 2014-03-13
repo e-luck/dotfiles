6526dfdea14f9511ea535230975945be8ea4e69b
e-luck <eiji.fukushima@gmail.com>
2014-03-13 20:01:44 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DOT_FILES=( .gitconfig .gitignore_global .gvimrc .tmuxconf .vim .vimrc .zshrc )
+
+for file in ${DOT_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+# Install Homebrew
+which -s brew
+
+if [[ $? != 0 ]]
+then
+	echo 'Installing Homebrew…'
+	ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+#stop on error
+set -e
+
+# Make sure we’re using the latest Homebrew
+echo 'Update hombrew…'
+brew update
+
+# Install formula
+brew bundle
+
+# install oh-my-zsh
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
